3da0cba98568b70fd7608af8aa480e90e5c43127
Imelda Viridiana García Cano <50310566+viridianacano@users.noreply.github.com>
2023-05-02 02:53:16 -0600

    Change database info

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports={
     PORT: process.env.PORT,
 
         DB:{
-            PORT: process.env.DB_PORT ||5432,
+            PORT: process.env.DB_PORT || 5432,
             HOST: process.env.DB_HOST,
             USER: process.env.DB_USER,
             NAME: process.env.DB_NAME,
